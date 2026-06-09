ab2d828054a34a0eee6a5bcd1a2b56e50fc8c5dc
AnkitGurung <ankitgurung234@gmail.com>
2026-06-09 11:58:00 +0545

    add 2 more students

--- src/main/java/com/mainbranch/Student.java
+++ src/main/java/com/mainbranch/Student.java
@@ -1,10 +1,14 @@
 package com.mainbranch;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     private Long id;
     private String name;
