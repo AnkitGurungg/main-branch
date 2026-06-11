7cb7a331d5e8d952f8639e1c02026a5f9e7bdb45
AnkitGurung <ankitgurung234@gmail.com>
2026-06-11 11:41:54 +0545

    add hello word

--- src/main/java/com/mainbranch/MainBranchApplication.java
+++ src/main/java/com/mainbranch/MainBranchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainBranchApplication {
 
     public static void main(String[] args) {
+        System.err.println("Hello, World!");
         SpringApplication.run(MainBranchApplication.class, args);
     }
 
